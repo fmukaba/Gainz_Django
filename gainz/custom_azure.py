157201c345405f06b42dd7ecb91e919231138e9b
Francois (Franck) Mukaba <fxkikomina@gmail.com>
2020-11-07 23:20:46 -0800

    Update custom_azure.py

--- gainz/custom_azure.py
+++ gainz/custom_azure.py
@@ -2,6 +2,6 @@ from storages.backends.azure_storage import AzureStorage
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'getgainzstorage'
-    account_key = 'K7V1Nsx0KU0XxXD0UjdUaaeT9mZQW4qbdHmhUJycrKce/+BTVpqNtxuUMAiGSAsgW69q7BM1KDCsgMz2XdtgpQ==' # Must be replaced by your <storage_account_key>
+    account_key = '' # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
